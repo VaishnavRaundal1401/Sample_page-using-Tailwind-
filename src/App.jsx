8794ae4736a81219cc8fc21c7ceedb1855dce987
Vaishnav Raundal <vaishnavraundal2016@gmail.com>
2024-07-30 19:25:34 +0530

    added the responsive feature section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import Feature from "./components/Feature"
+import HeroSection from "./components/HeroSection"
 import Navbar from "./components/Navbar"
 
 const App = () => {
   return (
     <>
       <Navbar/>
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <HeroSection/>
+        <Feature/>
+      </div>
     </>
   )
 }
