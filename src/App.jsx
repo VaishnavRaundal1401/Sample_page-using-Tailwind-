8f8dd3e416bb193aec4139760f2dd39f83243080
Vaishnav Raundal <vaishnavraundal2016@gmail.com>
2024-07-31 10:28:13 +0530

    made the pricing and workflow section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import Feature from "./components/Feature"
 import HeroSection from "./components/HeroSection"
 import Navbar from "./components/Navbar"
+import Price from "./components/Price"
+import WorkflowSection from "./components/WorkflowSection"
 
 const App = () => {
   return (
@@ -9,6 +11,8 @@ const App = () => {
       <div className="max-w-7xl mx-auto pt-20 px-6">
         <HeroSection/>
         <Feature/>
+        <WorkflowSection/>
+        <Price/>
       </div>
     </>
   )
